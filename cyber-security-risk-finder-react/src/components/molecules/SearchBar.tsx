798df7939d60399367674114cea585d0130dd98f
Mauricio Montoya Medrano <mcubico33@gmail.com>
2023-06-27 11:17:57 -0500

    Agrego el componente que se usará para realizar las búsquedas

---
+++ cyber-security-risk-finder-react/src/components/molecules/SearchBar.tsx
@@ -0,0 +1,57 @@
+import { FC, useRef } from 'react';
+import {
+  Box,
+  Container,
+  IconButton,
+  TextField,
+  TextFieldProps,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+type TSearchBar = {
+  onSearchHandle(keyword: string): void
+}
+
+const SearchBar: FC<TSearchBar> = (data) => {
+  const searchRefInput = useRef<TextFieldProps>()
+
+  const setKeywordSearchHandle = (): void => {
+    const keyword: string = searchRefInput?.current?.value as string
+    data.onSearchHandle(keyword)
+  }
+
+  return <>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <form>
+          <TextField
+            id="search-bar"
+            className="text"
+            onChange={setKeywordSearchHandle}
+            label="Enter a keyword"
+            variant="outlined"
+            placeholder="Search..."
+            size="small"
+            inputRef={searchRefInput}
+          />
+          <IconButton
+            type="submit"
+            aria-label="search"
+            onClick={setKeywordSearchHandle}
+          >
+            <SearchIcon />
+          </IconButton>
+        </form>
+      </Box>
+    </Container>
+  </>
+}
+
+export default SearchBar
